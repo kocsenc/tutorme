d08c4f5caab3543c0868d511835f515a19b1e685
Kocsen <kxc4519@rit.edu>
2014-04-09 20:03:42 -0400

    Login page revamp TutorMeUser implements pacelable

--- app/src/main/java/edu/rit/se/tutorme/api/TutorMeUser.java
+++ app/src/main/java/edu/rit/se/tutorme/api/TutorMeUser.java
@@ -12,14 +12,32 @@ import org.json.JSONObject;
 public class TutorMeUser implements Parcelable {
 
     private UserType userType;
+    private JSONObject userJson;
     private String name;
     private String email;
 
+
     /**
      * Default constructor.
+     *
      * @param userData
      */
     public TutorMeUser(JSONObject userData) {
+        this.userJson = userData;
+
+        try {
+            this.userType = UserType.toUserType(userData.get("type").toString());
+            this.name = (String) userData.get("name");
+            this.email = (String) userData.get("email");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TutorMeUser(Parcel in) {
+        JSONObject userData = (JSONObject) in.readValue(TutorMeUser.class.getClassLoader());
+        this.userJson = userData;
+
         try {
             this.userType = UserType.toUserType(userData.get("type").toString());
             this.name = (String) userData.get("name");
@@ -41,6 +59,11 @@ public class TutorMeUser implements Parcelable {
         return email;
     }
 
+
+    public JSONObject getJSON() {
+        return userJson;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -48,6 +71,18 @@ public class TutorMeUser implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeArray(new String[] {this.name, this.email});
+        parcel.writeValue(this.userJson);
     }
+
+    public static final Parcelable.Creator<TutorMeUser> CREATOR = new Parcelable.Creator<TutorMeUser>() {
+        public TutorMeUser createFromParcel(Parcel in) {
+            return new TutorMeUser(in);
+        }
+
+        public TutorMeUser[] newArray(int size) {
+            return new TutorMeUser[size];
+        }
+    };
+
+
 }
